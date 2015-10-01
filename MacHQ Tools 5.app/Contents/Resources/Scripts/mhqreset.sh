1e9a473bc0428475e98e4610e3abfe83f7187699
Nick <nilness@gmail.com>
2015-10-01 12:18:47 -0500

    10.11 fixes
    Updated version detection for 10.11

--- MacHQ Tools 5.app/Contents/Resources/Scripts/mhqreset.sh
+++ MacHQ Tools 5.app/Contents/Resources/Scripts/mhqreset.sh
@@ -45,7 +45,7 @@ case `/usr/bin/sw_vers -productVersion | /usr/bin/awk -F . '{print $2}'` in
 
        dscl . -delete /users/"${USER_NAME}"
        ;;
-  [789] | 10) rm /var/db/dslocal/nodes/Default/users/"${USER_NAME}".plist
+  [789] | 10 | 11) rm /var/db/dslocal/nodes/Default/users/"${USER_NAME}".plist
        ;; 
     *) echo "Unrecognized System Version - user account was NOT removed"
        exit
