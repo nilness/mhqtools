aaa2551be3d41504a569c6c09e7536ddd603bfdd
Nick <nilness@gmail.com>
2018-02-26 14:54:37 -0600

    Include full paths
    Added full paths for commans

--- MacHQ Tools 5.app/Contents/Resources/Scripts/mhqreset.sh
+++ MacHQ Tools 5.app/Contents/Resources/Scripts/mhqreset.sh
@@ -11,37 +11,39 @@ fi
 # try to verify single-user mode!
 
 if [ `! sysctl -n kern.singleuser` != "1" ]; then
-    echo Script must be run in single user mode
+    /bin/echo Script must be run in single user mode
     exit
 fi
 
 # check and mount the hard drive
-echo "********************************* Mounting file system"
+/bin/echo "********************************* Mounting file system"
 /sbin/fsck -fy
 /sbin/mount -uw /
 
 
 # we remove the user account differently based on which version of the OS we're running on
-echo "********************************* Removing user account"
+/bin/echo "********************************* Removing user account"
 case $(/usr/bin/sw_vers -productVersion | /usr/bin/awk -F . '{print $2}') in
 
-   [34]) rm -rf /var/db/netinfo/local.nidb ### remove the netinfo db file in 10.3 & 10.4
+   [34]) /bin/rm -rf /var/db/netinfo/local.nidb 
+   ### remove the netinfo db file in 10.3 & 10.4
        ;;
-   [56]) launchctl load /System/Library/LaunchDaemons/com.apple.DirectoryServices.plist ### start directory services
+   [56]) /bin/launchctl load /System/Library/LaunchDaemons/com.apple.DirectoryServices.plist 
+   ### start directory services
 
        # remove user from directory services
-       dscl . -delete /groups/_appserveradm GroupMembership "${USER_NAME}"
-       dscl . -delete /groups/_appserverusr GroupMembership "${USER_NAME}"
-       dscl . -delete /groups/_lpadmin GroupMembership "${USER_NAME}"
-       dscl . -delete /groups/admin GroupMembership "${USER_NAME}"
-       dscl . -delete /groups/com.apple.sharepoint.group.1 GroupMembership "${USER_NAME}"
-       dscl . -delete /groups/staff GroupMembership "${USER_NAME}"
+       /usr/bin/dscl . -delete /groups/_appserveradm GroupMembership "${USER_NAME}"
+       /usr/bin/dscl . -delete /groups/_appserverusr GroupMembership "${USER_NAME}"
+       /usr/bin/dscl . -delete /groups/_lpadmin GroupMembership "${USER_NAME}"
+       /usr/bin/dscl . -delete /groups/admin GroupMembership "${USER_NAME}"
+       /usr/bin/dscl . -delete /groups/com.apple.sharepoint.group.1 GroupMembership "${USER_NAME}"
+       /usr/bin/dscl . -delete /groups/staff GroupMembership "${USER_NAME}"
 
-       dscl . -delete /users/"${USER_NAME}"
+       /usr/bin/dscl . -delete /users/"${USER_NAME}"
        ;;
-  [789] | 10 | 11 | 12 | 13) rm /var/db/dslocal/nodes/Default/users/"${USER_NAME}".plist
+  [789] | 10 | 11 | 12 | 13) /bin/rm /var/db/dslocal/nodes/Default/users/"${USER_NAME}".plist
        ;; 
-    *) echo "Unrecognized System Version - user account was NOT removed"
+    *) /bin/echo "Unrecognized System Version - user account was NOT removed"
        exit
        ;;
 esac
@@ -49,89 +51,95 @@ esac
 # further commands will run in all recognized systems
 
 if [ "${USER_NAME}" == "tech" ]; then
-echo "********************************* Removing tech user additional items"
+/bin/echo "********************************* Removing tech user additional items"
 # delete various files installed by tech apps
-rm -rf /Library/Application\ Support/CrashReporter
-rm -rf /Library/Application\ Support/DriveGenius
-rm -rf /Library/Application\ Support/Roxio
-rm /Library/LaunchAgents/com.prosofteng.DGMonitor.plist
-rm /Library/LaunchDaemons/com.bombich.ccc.plist
-rm /Library/LaunchDaemons/com.prosofteng.DriveGenius.locum
-rm -rf /Library/NTFSProgs
-rm /Library/Preferences/com.volitans-software.smartutility.plist
-rm /Library/PrivilegedHelperTools/com.bombich.ccc
-rm /Library/PrivilegedHelperTools/com.prosofteng.DriveGenius.locum
-rm -rf /Applications/Toast\ 11\ Titanium
+/bin/rm -rf /Library/Application\ Support/CrashReporter
+/bin/rm -rf /Library/Application\ Support/DriveGenius
+/bin/rm -rf /Library/Application\ Support/Roxio
+/bin/rm /Library/LaunchAgents/com.prosofteng.DGMonitor.plist
+/bin/rm /Library/LaunchDaemons/com.bombich.ccc.plist
+/bin/rm /Library/LaunchDaemons/com.prosofteng.DriveGenius.locum
+/bin/rm -rf /Library/NTFSProgs
+/bin/rm /Library/Preferences/com.volitans-software.smartutility.plist
+/bin/rm /Library/PrivilegedHelperTools/com.bombich.ccc
+/bin/rm /Library/PrivilegedHelperTools/com.prosofteng.DriveGenius.locum
+/bin/rm -rf /Applications/Toast\ 11\ Titanium
 fi
 
 # delete user folder
-echo "********************************* Deleting user folder"
-rm -rf /Users/"${USER_NAME}"
+/bin/echo "********************************* Deleting user folder"
+/bin/rm -rf /Users/"${USER_NAME}"
 
 # remove setup done file so setup runs on boot
-echo "********************************* Re-enabling Setup Assistant"
-rm /var/db/.AppleSetupDone
+/bin/echo "********************************* Re-enabling Setup Assistant"
+/bin/rm /var/db/.AppleSetupDone
 
 # remove network configuration files
-echo "********************************* Removing network configuration"
-rm -rf /Library/Preferences/SystemConfiguration/*
+/bin/echo "********************************* Removing network configuration"
+/bin/rm -rf /Library/Preferences/SystemConfiguration/*
 
 # remove machq power settings & schedules
-#echo "********************************* Removing power settings & schedules"
-#rm /Library/Preferences/SystemConfiguration/com.apple.PowerManagement.plist
-#rm /Library/Preferences/SystemConfiguration/com.apple.AutoWake.plist
+/bin/echo "********************************* Removing power settings & schedules"
+/bin/rm /Library/Preferences/SystemConfiguration/com.apple.PowerManagement.plist
+/bin/rm /Library/Preferences/SystemConfiguration/com.apple.AutoWake.plist
 
 # remove mhq startup item if present
-echo "********************************* Removing MacHQ Startup Item"
-rm -rf /Library/StartupItems/Set\ SWUpdate\ Server/
+/bin/echo "********************************* Removing MacHQ Startup Item"
+/bin/rm -rf /Library/StartupItems/Set\ SWUpdate\ Server/
 
 # remove machq swupdate server setting
-echo "********************************* Removing Software Update Server settings"
+/bin/echo "********************************* Removing Software Update Server settings"
 defaults delete /Library/Preferences/com.apple.SoftwareUpdate CatalogURL
 
 # remove machq launch agents and daemons
-echo "********************************* Removing launch agents and daemons"
-rm /Library/LaunchDaemons/com.machq.*
-rm /Library/LaunchAgents/com.machq.*
+/bin/echo "********************************* Removing launch agents and daemons"
+/bin/rm /Library/LaunchDaemons/com.machq.*
+/bin/rm /Library/LaunchAgents/com.machq.*
 
 # remove Outset
-launchctl unload "/Library/LaunchDaemons/com.github.outset.boot.plist"
-rm -fv "/Library/LaunchDaemons/com.github.outset.boot.plist"
-launchctl unload "/Library/LaunchDaemons/com.github.outset.cleanup.plist"
-rm -fv "/Library/LaunchDaemons/com.github.outset.cleanup.plist"
-launchctl unload "/Library/LaunchAgents/com.github.outset.login.plist"
-rm -fv "/Library/LaunchAgents/com.github.outset.login.plist"
-launchctl unload "/Library/LaunchAgents/com.github.outset.on-demand.plist"
-rm -fv "/Library/LaunchAgents/com.github.outset.on-demand.plist"
-rm -rfv /usr/local/outset
-pkgutil --forget com.github.outset
+/bin/echo "********************************* Removing Outcast daemons and agents"
+/bin/launchctl unload "/Library/LaunchDaemons/com.github.outset.boot.plist"
+/bin/rm -fv "/Library/LaunchDaemons/com.github.outset.boot.plist"
+/bin/launchctl unload "/Library/LaunchDaemons/com.github.outset.cleanup.plist"
+/bin/rm -fv "/Library/LaunchDaemons/com.github.outset.cleanup.plist"
+/bin/launchctl unload "/Library/LaunchAgents/com.github.outset.login.plist"
+/bin/rm -fv "/Library/LaunchAgents/com.github.outset.login.plist"
+/bin/launchctl unload "/Library/LaunchAgents/com.github.outset.on-demand.plist"
+/bin/rm -fv "/Library/LaunchAgents/com.github.outset.on-demand.plist"
+/bin/rm -rfv /usr/local/outset
+/usr/sbin/pkgutil --forget com.github.outset
 
 #remove Outset files
-rm -rf "/usr/local/outset"
+/bin/echo "********************************* Removing Outcast files"
+/bin/rm -rf "/usr/local/outset"
 
 #remove MacHQ WiFi profile
+/bin/echo "********************************* Removing MacHQ WiFi profile"
 /usr/bin/profiles -R -p machq.A84EA65A-F042-40D5-A9DE-E503F5215357
 
+
 #remove MacHQ Homepage profile
+/bin/echo "********************************* Removing MacHQ homepage profile"
 /usr/bin/profiles -R -p com.machq.profile.safari
 
 #reset power settings
-pmset restoredefaults
-pmset repeat cancel
+/bin/echo "********************************* Removing MacHQ power settings"
+/usr/bin/pmset restoredefaults
+/usr/bin/pmset repeat cancel
 
 # remove machq /usr/local folder
-echo "********************************* Removing /usr/local/mhq/"
-rm -rf /usr/local/mhq/
+/bin/echo "********************************* Removing /usr/local/mhq/"
+/bin/rm -rf /usr/local/mhq/
 
 # remove this script & root user .profile
-echo "********************************* Removing this script"
-rm "$0"
-rm /private/var/root/.profile
-rm /private/var/root/.bashrc
+/bin/echo "********************************* Removing this script"
+/bin/rm "$0"
+/bin/rm /private/var/root/.profile
+/bin/rm /private/var/root/.bashrc
 
 # clear any nvram arguments
-echo "********************************* Clearing nvram arguments"
-nvram boot-args=""
+/bin/echo "********************************* Clearing nvram arguments"
+/usr/sbin/nvram boot-args=""
 
 # shutdown the computer
-shutdown -h now
+/sbin/shutdown -h now
