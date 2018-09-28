bd29237ed6442073ca49bd23a3cc55e199fa7058
Nick <nilness@gmail.com>
2018-09-28 10:57:00 -0500

    Added NVMe Drive Support
    Adjusted renaming to consider NVMe drives

--- MacHQ Tools 5.app/Contents/Resources/Scripts/rename_hd_by_size.sh
+++ MacHQ Tools 5.app/Contents/Resources/Scripts/rename_hd_by_size.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-echo "rename_hd_by_size.sh - v1.1 ("`date`")"
+echo "rename_hd_by_size.sh - v1.2 ("`date`")"
 
 # Get the boot volume name
 boot_vol=$(bless --getBoot)
 
+echo echo boot_vol: ${boot_vol}
+
 # Get the device id for the boot volume
 boot_disk=$(diskutil info "${boot_vol}" | sed '/^ *Volume Name: */!d;s###')
 
+echo echo boot_disk: ${boot_disk}
+
 # Get the drive size from system profiler & round it
-sp_size=$(system_profiler SPSerialATADataType | sed '/^ *Capacity: */!d;s###' | awk '{print $1}' | head -n 1 | awk '{print int($1+0.5)}')
+sata_size=$(system_profiler SPSerialATADataType | sed '/^ *Capacity: */!d;s###' | awk '{print $1}' | head -n 1 | awk '{print int($1+0.5)}')
+
+echo echo sata_size: ${sata_size}
+
+nvme_size=$(system_profiler SPNVMeDataType | sed '/^ *Capacity: */!d;s###' | awk '{print $1}' | head -n 1 | awk '{print int($1+0.5)}')
+
+echo echo nvme_size: ${nvme_size}
+
+#combine the sata and nvme sizes to get one size
+
+sp_size=${sata_size}${nvme_size}
 
 echo echo sp_size: ${sp_size}
+
 
 # Get the drive size from Core Storage
 cs_size=$(diskutil cs list | awk '/Size/{print}' | sed -n -e 1p | tr '()' '\n' | head -2 | tail -1 | awk '{print $1}')
@@ -22,8 +37,23 @@ echo echo cs_size: ${cs_size}
 # Test for fusion drive
 fusion=$(diskutil list | grep Fusion)
 
+echo echo fusion: ${fusion}
+
 # Test for Apple SSD drive
-apple_ssd=$(system_profiler SPSerialATADataType | grep -i "Apple SSD")
+apple_sata_ssd=$(system_profiler SPSerialATADataType | grep -i "Apple SSD")
+
+echo echo apple_sata_ssd: ${apple_sata_ssd}
+
+apple_nvme_ssd=$(system_profiler SPNVMeDataType | grep -i "Apple SSD")
+
+echo echo apple_nvme_ssd: ${apple_nvme_ssd}
+
+#combine the sata and nvme SSD results to get one result
+
+apple_ssd=${apple_sata_ssd}${apple_nvme_ssd}
+
+echo echo apple_ssd: ${apple_ssd}
+
 
 # Get size for SSD drive
 if [ -z "${apple_ssd}" ]; then
